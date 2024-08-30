ac57c5379c6634debfc77c79dace2cf206f46b67
stella-055 <stellaongayo243@gmail.com>
2024-08-30 13:29:36 +0300

    server changes

--- src/server.js
+++ src/server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 
 
 app.use(cors({
-  origin: 'https://assortica.netlify.app/', // Replace with your frontend's origin
+  origin: 'https://assortica.netlify.app', // Replace with your frontend's origin
   methods: 'GET,POST,PUT,DELETE',
   credentials: true, // If you need to send cookies or authorization headers
 }));
